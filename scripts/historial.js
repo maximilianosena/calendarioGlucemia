6e9760cb1e53de8945802e179dbc1e8b63bcbc94
Maximiliano Sena <130717933+maximilianosena@users.noreply.github.com>
2024-10-06 23:57:23 -0300

    probando

--- scripts/historial.js
+++ scripts/historial.js
@@ -4,30 +4,8 @@
 /////////////////////////Valores al inicio////////////////////
 let container_registros = document.getElementById("container_registros")
 
-async function getValores() {
-    try {
-        let token = localStorage.getItem("token")
-        const userId = localStorage.getItem('id')
-        const response = await fetch(`https://backend-glucemia.vercel.app/all?userId=${userId}`, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        })
-
-
-        if (response.ok) {
-            const result = await response.json()
-            console.log(result)
-            view_resultados(result)
-        }
-    }
-    catch (e) {
-        console.error("Error: ", e)
-    }
-
-}
-getValores()
+/*
+*/
 
 function toDateTime(fecha, hora) {
     const [day, month, year] = fecha.split('-').map(Number);
@@ -38,12 +16,12 @@ function toDateTime(fecha, hora) {
 
 /////////////////////////Borrar registro////////////////////
 function view_resultados(array) {
-  // Ordenar por fecha y hora
-array.sort((a, b) => {
-    const dateA = toDateTime(a.fechaString, a.hora);
-    const dateB = toDateTime(b.fechaString, b.hora);
-    return dateB - dateA; // Ordenar de más reciente a más antiguo
-});
+    // Ordenar por fecha y hora
+    array.sort((a, b) => {
+        const dateA = toDateTime(a.fechaString, a.hora);
+        const dateB = toDateTime(b.fechaString, b.hora);
+        return dateB - dateA; // Ordenar de más reciente a más antiguo
+    });
 
     for (let i in array) {
         container_registros.innerHTML += ` <tr class=${color_row(array[i].valor)}>
@@ -55,13 +33,13 @@ array.sort((a, b) => {
                  <td><button id=${array[i].id} name="borrar">X</button></td>         
         </tr>`
     }
-  
+
     container_registros.addEventListener('click', (event) => {
-        if (event.target.name === 'borrar') {  
+        if (event.target.name === 'borrar') {
             const id = event.target.id;  // Obtenemos el id del botón
             console.log("Hago click en id: ", id);
-            
-           deleteRegistro(id);
+
+            deleteRegistro(id);
         }
     });
 
@@ -95,11 +73,63 @@ async function deleteRegistro(id) {
 
 }
 
-function color_row(valor){
-    if (valor > 180 || valor < 70){
-        return("table-danger")
+function color_row(valor) {
+    if (valor > 180 || valor < 70) {
+        return ("table-danger")
     } else {
-        return("table-success")
+        return ("table-success")
     }
 
 }
+
+let checkAlto = document.getElementById("checkAlto")
+let checkBajo = document.getElementById("checkBajo")
+let checkNormal = document.getElementById("checkNormal")
+let checkTodos = document.getElementById("todos")
+
+
+let valorMin, valorMax;
+let queryString = ''
+
+if (checkBajo.checked) {
+    const [valorMin, valorMax] = checkBajo.value.split('-')
+} else if
+    (checkAlto.checked) {
+    const [valorMin, valorMax] = checkAlto.value.split('-')
+} else if (checkNormal.checked)
+{
+    const [valorMin, valorMax] = checkNormal.value.split('-')
+} 
+
+
+if (checkTodos.checked) {
+    queryString = ''; // No se necesita query string para "todos"
+} else if (valorMin !== undefined && valorMax !== undefined) {
+    queryString = `?valorMin=${valorMin}&valorMax=${valorMax}`;
+}
+
+
+async function getValores() {
+    try {
+        let token = localStorage.getItem("token")
+        const userId = localStorage.getItem('id')
+        const response = await fetch(`https://backend-glucemia.vercel.app/all?userId=${userId}${queryString}}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+
+
+        if (response.ok) {
+            const result = await response.json()
+            console.log(result)
+            view_resultados(result)
+        }
+    }
+    catch (e) {
+        console.error("Error: ", e)
+    }
+
+}
+getValores()
